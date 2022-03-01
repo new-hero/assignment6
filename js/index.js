a535b4417fdfa37225bd0451a42c7fe9402df2be
subrata sarker <tccdcc8@gmail.com>
2022-03-02 01:35:28 +0600

    modify

--- js/index.js
+++ js/index.js
@@ -1,30 +1,72 @@
 const searchPhone = () => {
-    const searchText = document.getElementById('searchBox').value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhone(data.data))
+    const searchbox = document.getElementById('searchBox');
+    const searchText = searchbox.value.toLowerCase();
+
+    if (searchText == '') {
+        alert('Please write something')
+    }
+    else if (searchText < 0) {
+        alert('Write phone name')
+    }
+    else {
+        document.getElementById('spinner').style.display = 'block';
+
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhone(data.data))
+
+        searchbox.value = '';
+    }
+
+
+
 
 }
 const displayPhone = phones => {
+
     const containerDiv = document.getElementById('productList');
-    phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div class="col">
-            <div class="card h-100 rounded">
-                <img src="${phone.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${phone.phone_name}</h5>
-                    <h5 class="card-title">Brand: ${phone.brand}</h5>
-                    <a href="#" class="btn btn-primary" onclick="phoneDetailes('${phone.slug}')"> Detailes </a>
+    containerDiv.textContent = '';
+    const noPhone = document.getElementById('noPhone');
+    const seeMore = document.getElementById('seeMore');
+    seeMore.classList.add('d-none');
+
+    if (phones.length == 0) {
+        noPhone.classList.remove('d-none')
+    }
+    else {
+        noPhone.classList.add('d-none')
+        phones?.forEach(phone => {
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="col">
+                <div class="card h-100 rounded">
+                    <img src="${phone.image}" class="card-img-top p-4" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${phone.phone_name}</h5>
+                        <h5 class="card-title my-3">Brand: ${phone.brand}</h5>
+                        <a href="#" class="btn btn-success rounded-pill" onclick="phoneDetailes('${phone.slug}')"> Detailes </a>
+                    </div>
                 </div>
             </div>
-        </div>
-        `;
-        containerDiv.appendChild(div);
-    });
+            `;
+            if (containerDiv.childNodes.length >= 20) {
+                seeMore.classList.remove('d-none');
+                seeMore.addEventListener('click', () => {
+                    containerDiv.appendChild(div);
+                })
+            }
+            else {
+                containerDiv.appendChild(div);
+            }
+        });
+
+    }
+
+    document.getElementById('spinner').style.display = 'none';
+
 }
+
 
 const phoneDetailes = id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
@@ -35,85 +77,34 @@ const phoneDetailes = id => {
 
 const phoneDetailesData = phoneId => {
     const DetailesDiv = document.getElementById('phoneDetailes');
-
-
-
-
-
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="row mb-5 border border-3 p-3">
-    <div class="col">
-        <img src="${phoneId.image}" alt="" class="img-fluid" width="400px">
-    </div>
-    <div class="col">
-        <div class="text-start">
-            <h3>Name: ${phoneId.name}</h3>
-            <h3>Brand: ${phoneId.brand ? phoneId.brand : 'Brand No Found'}</h3>
-            <h5>Release Date: ${phoneId.releaseDate ? phoneId.releaseDate : 'Release Date Not Found'}</h5>
-            <h5>Storage: ${phoneId?.mainFeatures?.storage ? phoneId.mainFeatures.storage : 'Storage Not Found'}  </h5>
-            <h5>DisplaySize: ${phoneId?.mainFeatures.displaySize} </h5>
-             <h5>ChipSet: ${phoneId?.mainFeatures.chipSet} </h5>
-             <h5>Memory: ${phoneId?.mainFeatures.memory} </h5>
-             <h3>Sensors</h3>
-            
-             <h5>${phoneId?.mainFeatures?.sensors ? phoneId.mainFeatures.sensors : 'Not supported'}</h5>
-
-             <h3>Others </h3>
-             <h5>WLAN: ${phoneId.others ? phoneId.others.WLAN : 'Not'} </h5>
-             <h5>Bluetooth: ${phoneId.others ? phoneId.others.Bluetooth : 'Not Support'} </h5>
-             <h5>GPS: ${phoneId.others ? phoneId.others.GPS : 'Not Support'} </h5>
-             <h5>NFC: ${phoneId.others ? phoneId.others.NFC : 'Not Support'} </h5>
-             <h5>Radio: ${phoneId.other ? phoneId.others.Radio : 'Not Support '} </h5>
-             <h5>USB: ${phoneId.others ? phoneId.others.USB : 'Not Support'} </h5>
-             
+    <div class="row   p-3">
+        <div class="col">
+            <img src="${phoneId.image}" alt="" class="img-fluid my-5" width="400px">
         </div>
+        <div class="col">
+            <div class="text-start my-5">
+                <h3> ${phoneId.name}</h3>
+                <h4>Brand: ${phoneId.brand ? phoneId.brand : 'Brand No Found'}</h4>
+                <h5>Release Date: ${phoneId.releaseDate ? phoneId.releaseDate : 'Release Date Not Found'}</h5>
+                <h5>Storage: ${phoneId?.mainFeatures?.storage ? phoneId.mainFeatures.storage : 'Storage Not Found'}</h5>
+                <h5>DisplaySize: ${phoneId?.mainFeatures.displaySize} </h5>
+                <h5>ChipSet: ${phoneId?.mainFeatures.chipSet} </h5>
+                <h5>Memory: ${phoneId?.mainFeatures.memory} </h5>
+                <h3>Sensors</h3>
+                <h5>${phoneId?.mainFeatures?.sensors ? phoneId.mainFeatures.sensors : 'Not supported'}</h5>
+                <h3>Others </h3>
+                <h5>WLAN: ${phoneId.others ? phoneId.others.WLAN : 'Not Support'} </h5>
+                <h5>Bluetooth: ${phoneId.others ? phoneId.others.Bluetooth : 'Not Support'} </h5>
+                <h5>GPS: ${phoneId.others ? phoneId.others.GPS : 'Not Support'} </h5>
+                <h5>NFC: ${phoneId.others ? phoneId.others.NFC : 'Not Support'} </h5>
+                <h5>Radio: ${phoneId.other ? phoneId.others.Radio : 'Not Support '} </h5>
+                <h5>USB: ${phoneId.others ? phoneId.others.USB : 'Not Support'} </h5>
+            </div>
+        </div>
+        <hr class="border border-dark"> 
     </div>
-    </div>
-
     `;
     DetailesDiv.appendChild(div);
-
-
-
 }
-
-const getSensotList = sensors => {
-    // console.log(sensors)
-    const list = document.getElementById('list')
-    for (const sensor of sensors) {
-        const li = document.createElement('li');
-        li.innerText = sensor;
-        list.appendChild(li)
-
-    }
-
-
-
-    // const sensorList= li;
-}
-
-
-/* 
-    
-    <h6>Storage: ${phoneId.}  </h6>
-            <h6>DisplaySize: ${phoneId.} </h6>
-             <h6>ChipSet: ${phoneId.} </h6>
-             <h6>Memory: ${phoneId.} </h6>
-             <h2>Sensors </h2>
-             <h2>Others </h2>
-             <h6>WLAN: ${phoneId.} </h6>
-             <h6>Bluetooth: ${phoneId.} </h6>
-             <h6>GPS: ${phoneId.} </h6>
-             <h6>NFC: ${phoneId.} </h6>
-             <h6>Radio: ${phoneId.} </h6>
-             <h6>USB: ${phoneId.} </h6>
-
-             <h6>WLAN: ${phoneId.others.WLAN} </h6>
-             <h6>Bluetooth: ${phoneId.others} </h6>
-             <h6>GPS: ${phoneId.others} </h6>
-             <h6>NFC: ${phoneId.others} </h6>
-             <h6>Radio: ${phoneId.other} </h6>
-             <h6>USB: ${phoneId.others} </h6>
-
-*/
